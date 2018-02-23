8393fd2003fec0bf804d71fa91c1e39dc70a3712
fX Patrick Ryanto <patrick.ryanto@outlook.com>
2018-02-23 20:42:08 +0700

    init commit

---
+++ js/site.js
@@ -0,0 +1,109 @@
+//scrollrevealjs implementation
+window.sr = ScrollReveal();
+sr.reveal('.header h1', {
+    duration: 2000,
+    origin:'right',
+    distance:'300px',
+    reset: true
+});
+sr.reveal('.info-left1', {
+  duration: 2000,
+  origin:'left',
+  distance:'1000px',
+  viewFactor: 0.1,
+  reset: true
+});
+sr.reveal('.test', {
+  duration: 3000,
+  origin:'left',
+  distance:'1000px',
+  viewFactor: 0.1,
+  reset: true
+});  
+sr.reveal('.img-mission3', {
+  duration: 5000,
+  origin:'right',
+  distance:'300px',
+  reset: true
+});  
+
+
+// Typewriter effect
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 200;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+$(function() {
+          // Smooth Scrolling
+          $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
+
+          // Change after scroll
+          $(document).scroll(function () {
+            var $nav = $(".navbar-fixed-top");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > 400);
+          });
+        });
